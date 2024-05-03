dd9c6a8de1f888c48f2bc91ccbd7c6278092a735
Marquitoslopez <marcosariellopez4@gmail.com>
2024-05-02 22:17:05 -0300

    VERSION.4.0

--- tateti.php
+++ tateti.php
@@ -58,12 +58,12 @@ foreach (range('A', 'C') as $letra) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if (isset($_POST['posicion_jugador1'])) {
         $posicion_jugador1 = $_POST['posicion_jugador1'];
-        $jugador = '1';
+        $jugador = 'X';
         echo "Jugador 1 se posiciono en: " . $jugador. "\n";
     }
     if (isset($_POST['posicion_jugador2'])) {
         $posicion_jugador2 = $_POST['posicion_jugador2'];
-        $jugador2 = '2';
+        $jugador2 = 'O';
         echo "Jugador 2 se posiciono en: " . $jugador2 . "\n";
     }
 }
